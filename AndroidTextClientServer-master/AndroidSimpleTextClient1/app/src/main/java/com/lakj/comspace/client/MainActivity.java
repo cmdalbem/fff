10046477b9ef9e1f9f3884a664a81bf16ee61e57
Cristiano M. Dalbem <ninja.dalbem@gmail.com>
2015-01-03 18:55:37 +0100

    First commit
    Done with basic functionalities of Client and Server:
    - the client reads the device gyroscope sending the angles to the server
    - the server handles these angles and simulates keyboard key presses
    depending on them
    
    All connectivity is done using TCP.

---
+++ AndroidTextClientServer-master/AndroidSimpleTextClient1/app/src/main/java/com/lakj/comspace/client/MainActivity.java
@@ -0,0 +1,175 @@
+package com.lakj.comspace.client;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
+
+public class MainActivity extends Activity implements SensorEventListener {
+
+	private Socket client;
+	private PrintWriter printwriter;
+	private EditText ipAddressField;
+
+    private TextView aTextView;
+    private Switch aSwitch;
+    private SensorManager sManager;
+
+    private static final int REFRESH_RATE_MS = 500;
+    private static final String DEFAULT_SERVER_ADDRESS = "192.168.43.106";
+
+    private float orientX, orientY, orientZ;
+
+    // Handler for the delayed messages, which will work as a timer
+    private Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            // Do task here
+            SendMessage sendMessageTask = new SendMessage();
+            sendMessageTask.execute();
+
+            Log.w("log","tick");
+
+            // Order new refresh
+            if(aSwitch.isChecked()) {
+                mHandler.sendEmptyMessageDelayed(0, REFRESH_RATE_MS);
+            }
+        }
+    };
+
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+        Log.w("log","Initializing...");
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        ipAddressField = (EditText) findViewById(R.id.ipAddressField); // reference to the text field
+        ipAddressField.setText(DEFAULT_SERVER_ADDRESS);
+
+        //get the TextView from the layout file
+        aTextView = (TextView) findViewById(R.id.aTextView);
+        //get a hook to the sensor service
+        sManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        aSwitch = (Switch) findViewById(R.id.switch1);
+
+        mHandler.sendEmptyMessageDelayed(0, REFRESH_RATE_MS);
+
+
+//        upButton = (Button) findViewById(R.id.upArrow);
+//        upButton.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                messsage = "up";
+//                SendMessage sendMessageTask = new SendMessage();
+//                sendMessageTask.execute();
+//            }
+//        });
+    }
+
+    public void onSwitchClicked(View view) {
+        // Is the toggle on?
+        boolean on = ((Switch) view).isChecked();
+
+        if (on) {
+            mHandler.sendEmptyMessageDelayed(0, REFRESH_RATE_MS);
+        } else {
+            // Nothing.
+        }
+    }
+
+	private class SendMessage extends AsyncTask<Void, Void, Void> {
+
+		@Override
+		protected Void doInBackground(Void... params) {
+			try {
+				client = new Socket(ipAddressField.getText().toString(), 4444); // connect to the server
+				printwriter = new PrintWriter(client.getOutputStream(), true);
+
+                String msg = orientX + "," + orientY + "," + orientZ;
+                Log.w("log","Sending message: " + msg);
+                printwriter.write(msg); // write the message to output stream
+
+				printwriter.flush();
+				printwriter.close();
+				client.close(); // closing the connection
+
+			} catch (UnknownHostException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return null;
+		}
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.slimple_text_client, menu);
+		return true;
+	}
+
+    //when this Activity starts
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+		/*register the sensor listener to listen to the gyroscope sensor, use the
+		 * callbacks defined in this class, and gather the sensor information as
+		 * quick as possible*/
+        sManager.registerListener(this, sManager.getDefaultSensor(Sensor.TYPE_ORIENTATION), SensorManager.SENSOR_DELAY_FASTEST);
+    }
+
+    //When this Activity isn't visible anymore
+    @Override
+    protected void onStop()
+    {
+        //unregister the sensor listener
+        sManager.unregisterListener(this);
+        super.onStop();
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor arg0, int arg1)
+    {
+        //Do nothing
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event)
+    {
+        //if sensor is unreliable, return void
+        if (event.accuracy == SensorManager.SENSOR_STATUS_UNRELIABLE) {
+            return;
+        }
+
+        orientX = event.values[2];
+        orientY = event.values[1];
+        orientZ = event.values[0];
+
+        //else it will output the Roll, Pitch and Yawn values
+        aTextView.setText("Orientation X (Roll) :" + Float.toString(orientX) + "\n" +
+                "Orientation Y (Pitch) :" + Float.toString(orientY) + "\n" +
+                "Orientation Z (Yaw) :" + Float.toString(orientZ));
+    }
+
+}
